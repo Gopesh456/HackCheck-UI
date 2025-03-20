74f55083410d91fdf7850558af513ad0f7632222
Gopesh Janardhanan <68292507+Gopesh456@users.noreply.github.com>
2025-03-20 23:10:12 +0400

    Remove Chat link from Navbar and adjust logout button styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -89,35 +89,7 @@ const Navbar = () => {
             </Link>
           </motion.div>
 
-          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-            <Link
-              href="/chat"
-              className="flex items-center text-gray-300 transition-colors hover:text-white"
-            >
-              <svg
-                className="w-5 h-5 mr-1 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z"
-                  clipRule="evenodd"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span>Chat</span>
-            </Link>
-          </motion.div>
-
+          
           <Button 
             className="flex items-center px-5 font-medium"
             onClick={handleLogout}
